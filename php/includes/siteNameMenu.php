e3c8eb9ec5d06cc0df89388472a2ac209798d46a
john gambini <john.a.gambini@gmail.com>
2018-09-20 18:08:53 -0400

    Modified set_siteNameMenu function to take contentMenuTrigger id argument because I'm using it in two places for calliope-sutra-1.0 and the two instances need different ids. Also added siteNameMenu class name to the span tag. This helps when referenceing the children it contains.

--- php/includes/siteNameMenu.php
+++ php/includes/siteNameMenu.php
@@ -5,11 +5,15 @@
  * Copyright 2015 2016 2017 2018 by John Gambini
  *
  ---------------------------------------------------------------------------------------------*/
-function set_siteNameMenu($contentObj,$userObj) {
+function set_siteNameMenu($contentObj,$userObj, $id = NULL) {
+	
+	$menuTriggerId = 'contentMenuTrigger-1';
+	if($id != NULL)
+		$menuTriggerId = $id;
 	
 ?>	
-<span style="margin:auto 0.25;white-space:nowrap">
-	<a class="menuItem wb-icon wb-icon-menu" id="menuTrigger" style="text-decoration:none" href="javascript:void(0)" onclick="toggleMenu(event,'popupContentMenu','menuTrigger', 'right')"></a>
+<span class="siteNameMenu" style="margin:auto 0.25;white-space:nowrap">
+	<a class="menuItem wb-icon wb-icon-menu" id="<?php echo $menuTriggerId ?>" style="text-decoration:none" href="javascript:void(0)" onclick="toggleMenu(event,'popupContentMenu','<?php echo $menuTriggerId?>', 'right')"></a>
 	<a id="siteName" class="siteName" style="text-decoration:none;padding:0em 0em 0em 0.25em" href="<?php echo WEBAPP . "/" . substr($contentObj->lang,0,2)?>">
 		<em><?php echo APP_NAME?></em>
 	</a>
