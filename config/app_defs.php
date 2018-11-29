3f7446a93fa51a481c35c29d63b9f24fa3ce7da8
john gambini <john.a.gambini@gmail.com>
2018-11-29 01:22:33 -0500

    Added files to run app directly from workbench dir.

---
+++ config/app_defs.php
@@ -0,0 +1,41 @@
+﻿<?php
+/*----------------------------------------------------------------------
+ * app_defs.php
+ *
+ * Copyright 2015 2016 2017 2018 by John Gambini
+ *
+ ----------------------------------------------------------------------*/
+define('APP_NAME','Manîn A. Bocss');
+define('SITE_NAME', "http://localhost");
+define('DATABASE', 'workbench_1');
+define('THEME','default');
+define('SUBSITE_NAME', '/workbench');
+define('WEBAPP', SITE_NAME . SUBSITE_NAME);
+define('WORKBENCH_FOLDER', SUBSITE_NAME);
+//define('WORKBENCH_FOLDER', SITE_NAME . 'vendor/workbench/core');
+
+define( 'THEMEDIR', SUBSITE_NAME . '/themes');
+define('CONTENTDIR', SITE_NAME . SUBSITE_NAME . '/wb-content/' . DATABASE );
+define('PDFDIR', SITE_NAME . '/pdf');
+
+//for retrieving code from file system's point of view
+define('WEBROOT', $_SERVER["DOCUMENT_ROOT"]);
+define('SUBSITE_DIR', WEBROOT . "\\workbench");
+define('SOURCEDIR', ABSDIR . "\\themes");
+//define('WORKBENCH_DIR', SUBSITE_DIR . "\\vendor\\workbench\\core" );
+define('WORKBENCH_DIR', SUBSITE_DIR );
+
+//pdo connection information
+define('DB_HOST','127.0.0.1');
+define('DB_USER','root');
+define('DB_PASSWORD', '');
+define('DB_CHARSET','utf8');
+define('INCLUDE_SQL_IN_ERROR_MESSAGE', true );
+
+// for php-gettext
+define('LOCALE_DIR', WORKBENCH_DIR .'/locale');
+define('DEFAULT_LOCALE', 'en_US');
+
+
+define('DEBUG_VERBOSE', true);
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ABSDIR', dirname(__FILE__));
+require_once( ABSDIR . '\config\app_defs.php');
+
+require_once( WORKBENCH_DIR . '\php\load_workbench.php');
+
+?>
